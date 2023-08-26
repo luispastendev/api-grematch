0e14f648801f44ad0b402c741a289d9fa505857e
Luis Pastén <luisromero@neubox.net>
2023-08-26 13:42:34 -0600

    routes fixed

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->group('api', ['namespace' => 'App\API\V1'], static function ($routes) {
+    $routes->get('/', 'ApiSample::index');
+});
+
+$routes->get('/', 'ApiSample::index', ['namespace' => 'App\API\V1']);
